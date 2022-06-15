2db8e5c284985aa45447b018ea855bec714a64d8
LHuynh05 <lahuynh@hawaii.edu>
2022-06-14 15:22:28 -1000

    codwars solution number

---
+++ 8kyu/simple.multiplication.js
@@ -0,0 +1,12 @@
+// This kata is about multiplying a given number by 8 if it is an even number and by nine otherwise.
+
+
+
+
+
+
+// MY SOLUTION
+
+function simpleMultiplication(number){
+    return number % 2 === 0 ? number * 8 : number * 9
+}
