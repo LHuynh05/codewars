0cf32458f76703b4908fe4c3a0205c92c4eba4e1
LHuynh05 <lahuynh@hawaii.edu>
2022-06-30 15:10:04 -1000

    adding codewars string fundamentals solution

---
+++ 8kyu/remove-anchor-from-url.js
@@ -0,0 +1,22 @@
+// Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+
+// Examples
+// "www.codewars.com#about" --> "www.codewars.com"
+// "www.codewars.com?page=1" -->"www.codewars.com?page=1"
+
+
+
+
+
+
+
+
+// MY SOLUTION
+
+function removeUrlAnchor(url){
+    // TODO: complete
+    let result = url.split('#')[0]
+    return(result)
+    
+    
+  }
