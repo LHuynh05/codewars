7de49b4ae0c07b425bc2fa03aff84a85e24b1fec
LHuynh05 <lahuynh@hawaii.edu>
2022-07-17 17:43:33 -1000

    adding reduce method problem

---
+++ 8kyu/beginner-reduce-but-grow.js
@@ -0,0 +1,16 @@
+// Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+// [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+
+
+
+
+
+
+
+// MY SOLUTION
+
+function grow(x){
+    return x.reduce((a,c) => a * c, 1)
+  }
