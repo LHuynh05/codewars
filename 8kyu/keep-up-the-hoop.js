7dd1a41a9b70f9741c285258e0c8076cc4a6c40a
LHuynh05 <lahuynh@hawaii.edu>
2022-08-17 08:49:13 -1000

    adding ternary operator in solution of codewars

---
+++ 8kyu/keep-up-the-hoop.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+
+
+
+
+// MY SOLUTION
+function hoopCount(n){
+    // if alex gets 10 or more console.log "Great, now move on to tricks"
+  return (n < 10) ? "Keep at it until you get it" : "Great, now move on to tricks"                //your code goes here    
+ }
