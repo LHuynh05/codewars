1f366f6795890bc38411bb89ae109d86ffb2abb4
LHuynh05 <lahuynh@hawaii.edu>
2022-08-20 13:27:06 -1000

    adding solution with math max and for loop

---
+++ 7kyu/small-enough-beginner.js
@@ -0,0 +1,28 @@
+// You will be given an array and a limit value. You must check that all values in the array are below or equal to the limit value. If they are, return true. Else, return false.
+
+// You can assume all values in the array are numbers.
+
+
+
+
+
+
+
+
+// MY SOLUTION
+function smallEnough(a, limit){
+    for(let i = 0; i <= a.length; i++){
+      if(a[i] > limit){
+        return false
+      }
+    }
+    return true 
+  
+  }
+
+
+// ALTERNATE SOLUTION 
+
+function smallEnough(a, limit) {
+    return Math.max(...a) <= limit;
+  }
