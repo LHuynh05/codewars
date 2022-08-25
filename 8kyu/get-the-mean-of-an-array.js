6ce3d1525b8ee66518b534d9bea864f0b639ad9d
LHuynh05 <lahuynh@hawaii.edu>
2022-08-24 14:11:05 -1000

    adding codewars with math floor and reduce

---
+++ 8kyu/get-the-mean-of-an-array.js
@@ -0,0 +1,19 @@
+// It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+
+// Return the average of the given array rounded down to its nearest integer.
+
+// The array will never be empty.
+
+
+
+
+
+
+
+
+// MY SOLUTION
+function getAverage(marks){
+    //TODO : calculate the downward rounded average of the marks array
+    //t total = marks.reduce((a,c) => a + c, 0) / marks.length
+    return Math.floor(marks.reduce((a,c) => a + c, 0) / marks.length)
+  }
