1f91c2a7b10d4694a774e01e7a1831920f92c0ff
LHuynh05 <lahuynh@hawaii.edu>
2022-09-07 19:46:28 -1000

    added prep comments

--- 7kyu/testing-1-2-3.js
+++ 7kyu/testing-1-2-3.js
@@ -18,6 +18,14 @@
 
 // MY SOLUTION
 
+//PARAMETERS: an array of varying length containing string values
+//RESULTS: an arrays of string containing the original values with the line number and a colon + space in front
+//EXAMPLES: it("test", () => {
+    // assert.deepEqual(number([]), [], 'Empty array should return empty array');
+    // assert.deepEqual(number(["a", "b", "c"]), ["1: a", "2: b", "3: c"], 'Return the correct line numbers');
+//PSEUDO CODE: iterate over each array value and prepend the line number (starting with one), and add a colon and a space between the line number and the value
+
+
 var number=function(array){
     //your awesome code here
     return array.map((line, index)=>{
