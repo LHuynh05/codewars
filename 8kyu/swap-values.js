be4317821ff727099e03e7e95fbd5123254467f6
LHuynh05 <lahuynh@hawaii.edu>
2022-09-21 20:42:28 -1000

    using reverse method for codewars solution

---
+++ 8kyu/swap-values.js
@@ -0,0 +1,18 @@
+// I would like to be able to pass an array with two elements to my swapValues function to swap the values. However it appears that the values aren't changing.
+
+// Can you figure out what's wrong here?
+
+
+
+
+
+
+
+
+// MY SOLUTION
+
+  //parament an array with 2 elements
+//result : swap the elements with one another
+//example: Test.assertEquals(arr[0], 2, "Failed swapping numbers")
+//pseudocode: use reverse method to swap the values
+swapValues = (args) => args.reverse()
