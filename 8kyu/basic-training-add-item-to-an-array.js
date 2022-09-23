3adf093cdbbf4d31d4afee1762748440668a085f
LHuynh05 <lahuynh@hawaii.edu>
2022-09-23 09:16:36 -1000

    adding push method to solution

---
+++ 8kyu/basic-training-add-item-to-an-array.js
@@ -0,0 +1,17 @@
+// Add the value "codewars" to the websites array.
+// After your code executes the websites array should == ["codewars"]
+
+// The websites array has already been defined for you using the following code:
+
+// var websites = [];
+
+
+
+
+
+
+
+
+// SOLUTION
+
+websites.push('codewars')
