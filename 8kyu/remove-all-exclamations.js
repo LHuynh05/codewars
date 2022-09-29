e0d94b1ec5826e308dc855f4656cc5fd8c574711
LHuynh05 <lahuynh@hawaii.edu>
2022-09-28 23:49:13 -1000

    adding codewars solution

---
+++ 8kyu/remove-all-exclamations.js
@@ -0,0 +1,13 @@
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+
+
+
+
+
+
+
+// my solution
+function removeExclamationMarks(s) {
+    return s.split('!').join('');
+  }
